df2f0c44aade1d6ee720057cb5061ea8198e8e04
Siddharth Kekre <siddharthkekre@b01-aruba-authenticated-10-110-194-222.wlan.cpp.edu>
2022-04-23 06:16:12 -0700

    Code Optimised for Parallel Computation

--- Code/Main.py
+++ Code/Main.py
@@ -12,13 +12,9 @@ timelog_quick = {}
 timelog_merge = {}
 timelog_parallel = {}
 
-def dummy_parallel(arr,algo):
-	if algo=='QuickSort':
-		quickSort(arr,0,len(arr)-1)
-		return arr
-	elif algo=='MergeSort':
-		mergeSort(arr)
-		return arr
+def dummy_parallel(arr):
+	quickSort(arr,0,len(arr)-1)
+	return arr
 
 def dummy_sequential(data,algo):
     try:
@@ -43,38 +39,17 @@ def dummy_sequential(data,algo):
             timelog_merge[len(data)] = -1
             print('Sequential Merge {} Fail',len(data))
 
-def sequential_sort(flag):
-    if flag == True:
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            data = HundredThousand
-            executor.submit(dummy_sequential,data,'Quick')
-            data = OneMillion
-            executor.submit(dummy_sequential,data,'Quick')
-            data = TenMillion
-            executor.submit(dummy_sequential,data,'Quick')
-            data = HundredMillion
-            executor.submit(dummy_sequential,data,'Quick')
-            data = HundredThousand
-            executor.submit(dummy_sequential,data,'Merge')
-            data = OneMillion
-            executor.submit(dummy_sequential,data,'Merge')
-            data = TenMillion
-            executor.submit(dummy_sequential,data,'Merge')
-            data = HundredMillion
-            executor.submit(dummy_sequential,data,'Merge')
-    return timelog_quick, timelog_merge
+with concurrent.futures.ProcessPoolExecutor() as executor:
 
-def parallel_sort(data):
-    start = datetime.now()
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        data1 = executor.submit(dummy_parallel,data[:int(len(data)*0.25)],'QuickSort')
-        chunk1 = list(data1.result())
-        data2 = executor.submit(dummy_parallel,data[int(len(data)*0.25):int(len(data)*0.5)],'QuickSort')
-        chunk2 = list(data2.result())
-        data3 = executor.submit(dummy_parallel,data[int(len(data)*0.5):int(len(data)*0.75)],'QuickSort')
-        chunk3 = list(data3.result())
-        data4 = executor.submit(dummy_parallel,data[int(len(data)*0.75):],'QuickSort')
-        chunk4 = list(data4.result())
+    data = HundredThousand
+    data1 = executor.submit(dummy_parallel,data[:int(len(data)*0.25)])
+    chunk1 = list(data1.result())
+    data2 = executor.submit(dummy_parallel,data[int(len(data)*0.25):int(len(data)*0.5)])
+    chunk2 = list(data2.result())
+    data3 = executor.submit(dummy_parallel,data[int(len(data)*0.5):int(len(data)*0.75)])
+    chunk3 = list(data3.result())
+    data4 = executor.submit(dummy_parallel,data[int(len(data)*0.75):])
+    chunk4 = list(data4.result())
     final_data = chunk1+chunk2+chunk3+chunk4
     mergeSort(final_data)
     end = datetime.now()
@@ -86,11 +61,84 @@ def parallel_sort(data):
         timelog_parallel[len(data)] = -1
         print('Parallel Sort {} Fail',len(data))
 
-parallel_sort(HundredThousand)
-parallel_sort(OneMillion)
-parallel_sort(TenMillion)
-parallel_sort(HundredMillion)
-sequential_sort(True)
+    data = OneMillion
+    data1 = executor.submit(dummy_parallel,data[:int(len(data)*0.25)])
+    chunk1 = list(data1.result())
+    data2 = executor.submit(dummy_parallel,data[int(len(data)*0.25):int(len(data)*0.5)])
+    chunk2 = list(data2.result())
+    data3 = executor.submit(dummy_parallel,data[int(len(data)*0.5):int(len(data)*0.75)])
+    chunk3 = list(data3.result())
+    data4 = executor.submit(dummy_parallel,data[int(len(data)*0.75):])
+    chunk4 = list(data4.result())
+    final_data = chunk1+chunk2+chunk3+chunk4
+    mergeSort(final_data)
+    end = datetime.now()
+    if final_data==sorted(data):
+        print('Parallel Sort {} Done',len(data))
+        time = end - start
+        timelog_parallel[len(data)] = time.total_seconds()
+    else:
+        timelog_parallel[len(data)] = -1
+        print('Parallel Sort {} Fail',len(data))
+
+    data = TenMillion
+    data1 = executor.submit(dummy_parallel,data[:int(len(data)*0.25)])
+    chunk1 = list(data1.result())
+    data2 = executor.submit(dummy_parallel,data[int(len(data)*0.25):int(len(data)*0.5)])
+    chunk2 = list(data2.result())
+    data3 = executor.submit(dummy_parallel,data[int(len(data)*0.5):int(len(data)*0.75)])
+    chunk3 = list(data3.result())
+    data4 = executor.submit(dummy_parallel,data[int(len(data)*0.75):])
+    chunk4 = list(data4.result())
+    final_data = chunk1+chunk2+chunk3+chunk4
+    mergeSort(final_data)
+    end = datetime.now()
+    if final_data==sorted(data):
+        print('Parallel Sort {} Done',len(data))
+        time = end - start
+        timelog_parallel[len(data)] = time.total_seconds()
+    else:
+        timelog_parallel[len(data)] = -1
+        print('Parallel Sort {} Fail',len(data))
+
+    data = HundredMillion
+    data1 = executor.submit(dummy_parallel,data[:int(len(data)*0.25)])
+    chunk1 = list(data1.result())
+    data2 = executor.submit(dummy_parallel,data[int(len(data)*0.25):int(len(data)*0.5)])
+    chunk2 = list(data2.result())
+    data3 = executor.submit(dummy_parallel,data[int(len(data)*0.5):int(len(data)*0.75)])
+    chunk3 = list(data3.result())
+    data4 = executor.submit(dummy_parallel,data[int(len(data)*0.75):])
+    chunk4 = list(data4.result())
+    final_data = chunk1+chunk2+chunk3+chunk4
+    mergeSort(final_data)
+    end = datetime.now()
+    if final_data==sorted(data):
+        print('Parallel Sort {} Done',len(data))
+        time = end - start
+        timelog_parallel[len(data)] = time.total_seconds()
+    else:
+        timelog_parallel[len(data)] = -1
+        print('Parallel Sort {} Fail',len(data))
+
+    data = HundredThousand
+    executor.submit(dummy_sequential,data,'Quick')
+    data = OneMillion
+    executor.submit(dummy_sequential,data,'Quick')
+    data = TenMillion
+    executor.submit(dummy_sequential,data,'Quick')
+    data = HundredMillion
+    executor.submit(dummy_sequential,data,'Quick')
+
+    data = HundredThousand
+    executor.submit(dummy_sequential,data,'Merge')
+    data = OneMillion
+    executor.submit(dummy_sequential,data,'Merge')
+    data = TenMillion
+    executor.submit(dummy_sequential,data,'Merge')
+    data = HundredMillion
+    executor.submit(dummy_sequential,data,'Merge')
+
 
 x = ['Hundred Thousand','One Million','Ten Million','Hundred Million']
 y0 = [0 for _ in range(4)]
