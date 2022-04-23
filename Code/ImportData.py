4ea72dd6bc17a850f1e38c55a114ce7b82c850b3
Siddharth Kekre <siddharthkekre@b01-aruba-authenticated-10-110-194-222.wlan.cpp.edu>
2022-04-23 05:57:43 -0700

    Code Optimised for Multi Processing

---
+++ Code/ImportData.py
@@ -0,0 +1,21 @@
+import csv
+
+with open('../Datasets/HundredThousand.csv', newline='\n') as f:
+    reader = csv.reader(f)
+    HundredThousand = list(reader)
+    HundredThousand = [int(i[0]) for i in HundredThousand]
+
+with open('../Datasets/OneMillion.csv', newline='\n') as f:
+    reader = csv.reader(f)
+    OneMillion = list(reader)
+    OneMillion = [int(i[0]) for i in OneMillion]
+
+with open('../Datasets/TenMillion.csv', newline='\n') as f:
+    reader = csv.reader(f)
+    TenMillion = list(reader)
+    TenMillion = [int(i[0]) for i in TenMillion]
+
+with open('../Datasets/HundredMillion.csv', newline='\n') as f:
+    reader = csv.reader(f)
+    HundredMillion = list(reader)
+    HundredMillion = [int(i[0]) for i in HundredMillion]
--- Code/Main.py
+++ Code/Main.py
@@ -1,73 +1,106 @@
-import csv
 import sys
+import concurrent.futures
 import matplotlib.pyplot as plt
-from datetime import datetime
 from QuickSort import quickSort
 from MergeSort import mergeSort
-from ParallelSort import parallel_sort
+from datetime import datetime
+from ImportData import HundredThousand,OneMillion,TenMillion,HundredMillion
 
 sys.setrecursionlimit(10000)
 
-final_data = []
-timelog_parallel = []
-timelog_merge = []
-timelog_quick = []
+timelog_quick = {}
+timelog_merge = {}
+timelog_parallel = {}
 
-def sort_log(filename):
+def dummy_parallel(arr,algo):
+	if algo=='QuickSort':
+		quickSort(arr,0,len(arr)-1)
+		return arr
+	elif algo=='MergeSort':
+		mergeSort(arr)
+		return arr
 
-    with open('../Datasets/'+filename, newline='\n') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-        data = [int(i[0]) for i in data]
-    print('\n',filename,' Imported')
-
-    # Parallel Sorting
-    start = datetime.now()
-    if parallel_sort(data):
+def dummy_sequential(data,algo):
+    try:
+        start = datetime.now()
+        if algo=='Quick':
+            quickSort(data,0,len(data)-1)
+        elif algo=='Merge':
+            mergeSort(data)
         end = datetime.now()
         time = end - start
-        timelog_parallel.append(time.total_seconds())
+        if algo=='Quick':
+            timelog_quick[len(data)] = time.total_seconds()
+            print('Sequential Quick {} Done',len(data))
+        elif algo=='Merge':
+            timelog_merge[len(data)] = time.total_seconds()
+            print('Sequential Merge {} Done',len(data))
+    except:
+        if algo=='Quick':
+            timelog_quick[len(data)] = -1
+            print('Sequential Quick {} Fail',len(data))
+        elif algo=='Merge':
+            timelog_merge[len(data)] = -1
+            print('Sequential Merge {} Fail',len(data))
+
+def sequential_sort(flag):
+    if flag == True:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            data = HundredThousand
+            executor.submit(dummy_sequential,data,'Quick')
+            data = OneMillion
+            executor.submit(dummy_sequential,data,'Quick')
+            data = TenMillion
+            executor.submit(dummy_sequential,data,'Quick')
+            data = HundredMillion
+            executor.submit(dummy_sequential,data,'Quick')
+            data = HundredThousand
+            executor.submit(dummy_sequential,data,'Merge')
+            data = OneMillion
+            executor.submit(dummy_sequential,data,'Merge')
+            data = TenMillion
+            executor.submit(dummy_sequential,data,'Merge')
+            data = HundredMillion
+            executor.submit(dummy_sequential,data,'Merge')
+    return timelog_quick, timelog_merge
+
+def parallel_sort(data):
+    start = datetime.now()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        data1 = executor.submit(dummy_parallel,data[:int(len(data)*0.25)],'QuickSort')
+        chunk1 = list(data1.result())
+        data2 = executor.submit(dummy_parallel,data[int(len(data)*0.25):int(len(data)*0.5)],'QuickSort')
+        chunk2 = list(data2.result())
+        data3 = executor.submit(dummy_parallel,data[int(len(data)*0.5):int(len(data)*0.75)],'QuickSort')
+        chunk3 = list(data3.result())
+        data4 = executor.submit(dummy_parallel,data[int(len(data)*0.75):],'QuickSort')
+        chunk4 = list(data4.result())
+    final_data = chunk1+chunk2+chunk3+chunk4
+    mergeSort(final_data)
+    end = datetime.now()
+    if final_data==sorted(data):
+        print('Parallel Sort {} Done',len(data))
+        time = end - start
+        timelog_parallel[len(data)] = time.total_seconds()
     else:
-        timelog_parallel.append(-1)
-    print('Completed Parallel Sorting for ',filename)
+        timelog_parallel[len(data)] = -1
+        print('Parallel Sort {} Fail',len(data))
 
-    # Sequential Merge Sort
-    start = datetime.now()
-    final_data = data
-    try:
-        mergeSort(final_data)
-        end = datetime.now()
-        time = end - start
-        timelog_merge.append(time.total_seconds())
-    except:
-        timelog_merge.append(-1)
-    print('Completed Sequential Merge Sort for ',filename)
+parallel_sort(HundredThousand)
+parallel_sort(OneMillion)
+parallel_sort(TenMillion)
+parallel_sort(HundredMillion)
+sequential_sort(True)
 
-    # Sequential Quick Sort
-    start = datetime.now()
-    final_data = data
-    try:
-        quickSort(final_data,0,len(final_data)-1)
-        end = datetime.now()
-        time = end - start
-        timelog_quick.append(time.total_seconds())
-    except:
-        timelog_quick.append(-1)
-    print('Completed Sequential Quick Sort for ',filename)
-
-
-datasets = ['HundredThousand.csv','OneMillion.csv','TenMillion.csv','HundredMillion.csv']
-
-for files in datasets:
-    sort_log(files)
-
-# Plotting Time Logs for Each Step
 x = ['Hundred Thousand','One Million','Ten Million','Hundred Million']
-y0 = [0 for _ in range(len(datasets))]
+y0 = [0 for _ in range(4)]
+merge = [timelog_merge[100000],timelog_merge[1000000],timelog_merge[10000000],timelog_merge[100000000]]
+quick = [timelog_quick[100000],timelog_quick[1000000],timelog_quick[10000000],timelog_quick[100000000]]
+parallel = [timelog_parallel[100000],timelog_parallel[1000000],timelog_parallel[10000000],timelog_parallel[100000000]]
 plt.plot(x, y0, color='black', linestyle='--', label='Zero Line')
-plt.plot(x, timelog_parallel, color='red', label='Parallel Sort')
-plt.plot(x, timelog_merge, color='green', label='Sequential Merge Sort')
-plt.plot(x, timelog_quick, color='blue', label='Sequential Quick Sort')
+plt.plot(x, parallel, color='red', label='Parallel Sort')
+plt.plot(x, merge, color='green', label='Sequential Merge Sort')
+plt.plot(x, quick, color='blue', label='Sequential Quick Sort')
 plt.title('Total Execution Time of Each Step v/s Size of Data')
 plt.xlabel('Size of Dataset')
 plt.ylabel('Time Taken in Seconds')
