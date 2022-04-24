75298b70a61cad8a7e654b65362b94c2240e3f67
Siddharth Kekre <siddharthkekre@b01-aruba-authenticated-10-110-194-222.wlan.cpp.edu>
2022-04-23 18:21:06 -0700

    Updated Quick Sort Code

--- Code/QuickSort.py
+++ Code/QuickSort.py
@@ -7,32 +7,43 @@ sys.setrecursionlimit(20000000)
 parallel = False
 def quickSort(arr,parallel):
 
-    elements = len(arr)
-    
-    #Base case
-    if elements < 2:
-        return arr
-    
-    current_position = 0 #Position of the partitioning element
-
-    for i in range(1, elements): #Partitioning loop
-         if arr[i] <= arr[0]:
-              current_position += 1
-              temp = arr[i]
-              arr[i] = arr[current_position]
-              arr[current_position] = temp
-
-    temp = arr[0]
-    arr[0] = arr[current_position] 
-    arr[current_position] = temp #Brings pivot to it's appropriate position
-
-    if parallel==False:
+    if parallel == False:
+        elements = len(arr)
+        #Base case
+        if elements < 2:
+            return arr
+        current_position = 0 #Position of the partitioning element
+        for i in range(1, elements): #Partitioning loop
+            if arr[i] <= arr[0]:
+                current_position += 1
+                temp = arr[i]
+                arr[i] = arr[current_position]
+                arr[current_position] = temp
+        temp = arr[0]
+        arr[0] = arr[current_position] 
+        arr[current_position] = temp #Brings pivot to it's appropriate position
         left = quickSort(arr[0:current_position],False) #Sorts the elements to the left of pivot
         right = quickSort(arr[current_position+1:elements],False) #sorts the elements to the right of pivot
-    elif parallel==True:
+        arr = left + [arr[current_position]] + right #Merging everything together
+        return arr
+
+    elif parallel == True:
+        elements = len(arr)
+        #Base case
+        if elements < 2:
+            return arr
+        current_position = 0 #Position of the partitioning element
+        for i in range(1, elements): #Partitioning loop
+            if arr[i] <= arr[0]:
+                current_position += 1
+                temp = arr[i]
+                arr[i] = arr[current_position]
+                arr[current_position] = temp
+        temp = arr[0]
+        arr[0] = arr[current_position] 
+        arr[current_position] = temp #Brings pivot to it's appropriate position
         with concurrent.futures.ProcessPoolExecutor() as executor:
             left = executor.submit(quickSort,arr[0:current_position],True) #Sorts the elements to the left of pivot
             right = executor.submit(quickSort,arr[current_position+1:elements],True) #sorts the elements to the right of pivot
-    arr = left + [arr[current_position]] + right #Merging everything together
-    
-    return arr
+        arr = left + [arr[current_position]] + right #Merging everything together
+        return arr
