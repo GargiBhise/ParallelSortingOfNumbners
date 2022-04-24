9489a3fba8769a0b871fd4a1150428160b2eb864
Siddharth Kekre <siddharthkekre@b01-aruba-authenticated-10-110-194-222.wlan.cpp.edu>
2022-04-24 06:39:51 -0700

    Updated Function

--- Code/ImportData.py
+++ Code/ImportData.py
@@ -1,6 +1,4 @@
 import csv
-import concurrent.futures
-
 def read_data(filename):
     try:
         with open('../Datasets/'+filename, newline='\n') as f:
@@ -8,19 +6,5 @@ def read_data(filename):
             data = list(reader)
             data = [int(i[0]) for i in data]
     except:
-        print('Error Reading ',filename,' ; Returning Empty List.')
         data = []
     return data
-
-if __name__ == "__main__":
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        ht = executor.submit(read_data,'HundredThousand.csv')
-        om = executor.submit(read_data,'OneMillion.csv')
-        tm = executor.submit(read_data,'TenMillion.csv')
-        fm = executor.submit(read_data,'FiftyMillion.csv')
-        hm = executor.submit(read_data,'HundredMillion.csv')
-        HundredThousand = ht.result()
-        OneMillion = om.result()
-        TenMillion = tm.result()
-        FiftyMillion = fm.result()
-        HundredMillion = hm.result()
